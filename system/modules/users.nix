6d7bc7cc4fb6f26d4aa5c7f3b9d91f5f47faaae4
stellaratica <stellaratica>
2025-07-01 16:11:18 -0700

    fix: set initial passwords (oops)

--- system/modules/users.nix
+++ system/modules/users.nix
@@ -1,10 +1,12 @@
 {pkgs, ...}: {
   users.users.stellaratica = {
     isNormalUser = true;
+    initialHashedPassword = "$6$ucT/ZNWKI7Y4tQuJ$nzMl12scpx9pmKqZmeHJUVGx98PlGfz4aDMCKcNCkLi2HBZlrtoWvqisMkoUGWJTCgaI26hSVeAyRhkov4/CX0";
     shell = pkgs.zsh;
     extraGroups = [
       "wheel"
       "networkmanager"
     ];
   };
+  users.users.root.initialHashedPassword = "$6$wskptZ5YQeXsCOE8$Rs/I5K5eR4fEHFd02B5uMIQtspEiwUKr5rILjmJso7y9vk1czbc4iTVzCyZ/IlMpyudDae.UkpQqNx0xx.9Hu0";
 }
