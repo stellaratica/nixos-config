d5f802bdc353b54e31e3d4601c32108587785ec8
stellaratica <stellaratica>
2025-07-19 12:37:45 -0700

    fix: make asusd config declarative

---
+++ system/modules/asusd.nix
@@ -0,0 +1,7 @@
+{
+  services.asusd = {
+    asusdConfig.source = ../config/asusd/asusd.ron;
+    auraConfigs.aura_19b6.source = ../config/asusd/aura_19b6.ron;
+    fanCurvesConfig.source = ../config/asusd/fan_curves.ron;
+  };
+}
