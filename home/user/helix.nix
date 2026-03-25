36d84c89a1a1e48e4c1857aeed635c492dd50bee
stellaratica <stellaratica@gmail.com>
2026-03-24 22:55:51 -0700

    cant spell

--- home/user/helix.nix
+++ home/user/helix.nix
@@ -330,7 +330,7 @@
         };
     };
     settings = {
-      theme = "catpuccin_mocha";
+      theme = "catppuccin_mocha";
       editor = {
         line-number = "relative";
         cursorline = true;
