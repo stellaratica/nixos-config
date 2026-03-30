ac77d8f68c0c1f175e429214c7966e7e29b842d4
stellaratica <stellaratica>
2026-03-30 15:37:36 -0700

    ly has cool shaders fun fact

--- system/modules/desktop.nix
+++ system/modules/desktop.nix
@@ -1,5 +1,33 @@
 {
   programs.hyprland.enable = true;
 
-  services.displayManager.ly.enable = true;
+  console.colors = [
+      "1e1e2e"
+      "f38ba8"
+      "a6e3a1"
+      "f9e2af"
+      "89b4fa"
+      "f5c2e7"
+      "94e2d5"
+      "a6adc8"
+      "585b70"
+      "f582a0"
+      "95e096"
+      "f6de9d"
+      "83aefe"
+      "f8b9e6"
+      "7be0d2"
+      "bac2de"
+  ];
+
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      animation = "colormix";
+      bigclock = "en";
+      colormix_col1 = "0x0005";
+      colormix_col2 = "0x0006";
+      colormix_col3 = "0x0001";
+    };
+  };
 }
