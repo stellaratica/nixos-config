47e38699fd6d54b9003d66931cdb7767f8985a5f
stellaratica <stellaratica>
2026-03-30 15:40:58 -0700

    cool little bootup/shutdown screen

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -13,4 +13,14 @@
     # enable = true;
     pkiBundle = "/var/lib/sbctl";
   };
+
+  boot.plymouth = {
+    enable = true;
+    themePackages = with pkgs; [
+      (adi1090x-plymouth-themes.override {
+        selected_themes = ["cuts_alt" "seal_3"];
+      })
+    ];
+    theme = "cuts_alt";
+  };
 }
