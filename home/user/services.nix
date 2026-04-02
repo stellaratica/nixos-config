6b4c20b2f09b86d67467f6c708225144883a41f0
stellaratica <stellaratica@gmail.com>
2026-04-02 13:48:49 -0700

    i dont use flameshot anymore

--- home/user/services.nix
+++ home/user/services.nix
@@ -1,19 +1,9 @@
-{inputs, ...}: {
-  imports = [inputs.nix-flatpak.homeManagerModules.nix-flatpak];
+{ inputs, ... }:
+{
+  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
   services = {
     clipse.enable = true;
-    flameshot = {
-      enable = true;
-      settings = {
-        General = {
-          disabledGrimWarning = true;
-          showHelp = false;
-          showSidePanelButton = false;
-          showDesktopNotification = false;
-        };
-      };
-    };
-    flatpak.packages = ["org.vinegarhq.Sober"];
+    flatpak.packages = [ "org.vinegarhq.Sober" ];
     playerctld.enable = true;
   };
 }
