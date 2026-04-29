e5c2e18dfad6c01d7d576ea5ea955e2e39d42474
stellaratica <stellaratica>
2026-04-28 20:01:55 -0700

    systemd stage 1 no longer supports boot.initrd.postResumeCommands

--- system/modules/impermanence.nix
+++ system/modules/impermanence.nix
@@ -1,9 +1,10 @@
 {
   inputs,
-  lib,
+  pkgs,
   ...
-}: {
-  imports = [inputs.impermanence.nixosModules.impermanence];
+}:
+{
+  imports = [ inputs.impermanence.nixosModules.impermanence ];
 
   environment.persistence."/persist" = {
     hideMounts = true;
@@ -38,9 +39,18 @@
         ".local/share/Steam"
         ".local/state/nvim"
         ".rustup"
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
+        {
+          directory = ".ssh";
+          mode = "0700";
+        }
+        {
+          directory = ".gnupg";
+          mode = "0700";
+        }
+        {
+          directory = ".local/share/keyrings";
+          mode = "0700";
+        }
       ];
       files = [
         ".p10k.zsh"
@@ -48,28 +58,69 @@
     };
   };
 
-  boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/root_vg/root /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
+  boot.initrd.systemd = {
+    services.impermance-btrfs-rolling-root = {
+      description = "Archiving existing BTRFS root subvolume and creating a fresh one";
+      # Specify dependencies explicitly
+      unitConfig.DefaultDependencies = false;
+      # The script needs to run to completion before this service is done
+      serviceConfig = {
+        Type = "oneshot";
+        # NOTE: to be able to see errors in your script do this
+        # StandardOutput = "journal+console";
+        # StandardError = "journal+console";
+      };
+      # This service is required for boot to succeed
+      requiredBy = [ "initrd.target" ];
+      # Should complete before any file systems are mounted
+      before = [ "sysroot.mount" ];
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
+      # Wait until the root device is available
+      # If you're altering a different device, specify its device unit explicitly.
+      # see: systemd-escape(1)
+      requires = [ "initrd-root-device.target" ];
+      after = [
+        "initrd-root-device.target"
+        # Allow hibernation to resume before trying to alter any data
+        "local-fs-pre.target"
+      ];
+
+      # The body of the script. Make your changes to data here
+      script = ''
+        mkdir /btrfs_tmp
+        mount /dev/root_vg/root /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+            mkdir -p /btrfs_tmp/old_roots
+            timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+            mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
+
+        delete_subvolume_recursively() {
+            IFS=$'\n'
+            for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                delete_subvolume_recursively "/btrfs_tmp/$i"
+            done
+            btrfs subvolume delete "$1"
+        }
+
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+            delete_subvolume_recursively "$i"
         done
-        btrfs subvolume delete "$1"
-    }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
+    extraBin = {
+      # "mkfs.ext4" = "${pkgs.e2fsprogs}/bin/mkfs.ext4";
+      "mkdir" = "${pkgs.coreutils}/bin/mkdir";
+      "date" = "${pkgs.coreutils}/bin/date";
+      "stat" = "${pkgs.coreutils}/bin/stat";
+      "mv" = "${pkgs.coreutils}/bin/mv";
+      "find" = "${pkgs.findutils}/bin/find";
+      "btrfs" = "${pkgs.btrfs-progs}/bin/btrfs";
+      # mount & umount already exist
+    }; # NOTE: path = [...]; doesnt work for initrd, use full paths in your script or extraBin
+  };
 
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
 }
